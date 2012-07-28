2733a32ce1ffa7d986b2937cb17133995caeed3a
Luis Perichon <luisperichon@gmail.com>
2012-07-28 18:32:23 -0300

    update plugin

--- wp-content/plugins/toppa-plugin-libraries-for-wordpress/ToppaSettings.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-interface ToppaSettings {
-    public function __get($name);
-    public function refresh();
-    public function set(array $newSettings, $preferExisting = false);
-    public function delete();
-}
